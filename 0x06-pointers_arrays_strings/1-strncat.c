6fe762c8053a186bcb130efda59e2069d871e1a9
abdelrahman <abdelrahman.habib74@gmail.com>
2023-10-20 05:17:21 -0700

    this is my commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *_strncat - function that concatenates two strings.
+ *
+ *@dest: pointer to destination input
+ *@src: pointer to source input
+ *@n: most number of pytes from @src 
+ *
+ *Return: @dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int c, j;
+/* j is a counter for  n bytes of src to be concatenated */
+/* length = length of destination string */
+
+	c  = 0;
+	while (dest[c])
+	{
+	     c++;
+	}
+	for (j = 0; j < n && src[j] !='\0'; j++,)
+	{
+		dest[c + j] = src[j];
+	}
+	dest[c + j] = '\0';
+	return (dest);
+}
