75de0d703dd9b831f32e4480bc47ffba9707dc24
abdelrahman <abdelrahman.habib74@gmail.com>
2023-10-22 13:42:02 -0700

    this is my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,19 @@
+/*
+ * File: 4-puts.c
+ * Auth: Brennans D Baraban <375@holbertonschool.com>
+ */
+
 #include <stdio.h>
- /**
-  * main - Entry point
-  *
-  * Description: A C program that prints with put function
-  *
-  * Return: Always 0 (success)
-  * */
+
+/**
+ * main - Print "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+/*
+ * File: 5-printf.c
+ * Auth: Brennan D Baraban
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Print "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new lines.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
